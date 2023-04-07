de762a8a350eb098657a8b330564c9a1fa998dbe
claracovitch <124149037+thclaracovitch@users.noreply.github.com>
2023-04-07 10:30:41 +0200

    Add files via upload

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thclarac <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: thclarac <thclarac@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 10:52:41 by thclarac          #+#    #+#             */
-/*   Updated: 2023/03/07 11:38:01 by thclarac         ###   ########.fr       */
+/*   Updated: 2023/04/07 10:28:37 by thclarac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
-# define BUFF_SIZE 5
-# define FD_SIZE 4096
+# define BUFFER_SIZE 5
+# define FD_SIZE 1024 
 
 # include <stdlib.h>
 # include <sys/types.h>
@@ -22,9 +22,9 @@
 
 char	*get_next_line(int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strchr (const char *string, int searchedchar);
+char	*ft_strchr(const char *string, int searchedchar);
 
-void	ft_bero(void *s, size_t n);
+void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t elementcount, size_t elementsize);
 
 size_t	ft_strlen(const char *str);
